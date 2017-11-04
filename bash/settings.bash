ec3967d44d04db6c44a52d912efbbe09c79d448a
CharliebWup <charilebwup@gmail.com>
2017-11-04 13:58:24 +0100

    Initial

--- bash/settings.bash
+++ bash/settings.bash
@@ -3,4 +3,10 @@ HISTFILE="$HOME/.bash_history"
 SAVEHIST=$HISTSIZE
 shopt -s histappend # append to history file
 
+# Enable checkwinsize to prevent garbage line-wrapping
+shopt -s checkwinsize
+
 export EDITOR=vim
+
+# tabs should be 4 chars long
+tabs 4
